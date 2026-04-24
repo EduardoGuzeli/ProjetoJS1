c88342050cdaf2dc0a495141cafee3a252d4a1f5
Eduardo <eduguzeli@gmail.com>
2026-04-24 09:48:30 -0300

    feat: html semântico

--- src/js/script.js
+++ src/js/script.js
@@ -29,3 +29,11 @@ const desconto = 0.15; //15%
 const precoFinal = precoProduto * (1 - desconto);
 console.log(precoFinal)
 console.log(`R$ ${precoFinal.toFixed(2)}`);
+
+const produto =[
+    {nome: "Teclado Mecânico", preco:200,promocao:true},
+    {nome: "Mouse Gamer", preco:300,promocao:false},
+    {nome: "Monitor Gamer", preco:900,promocao:true},
+    {nome: "Pad Mouse", preco:70,promocao:false},
+]
+console.log(produto)
